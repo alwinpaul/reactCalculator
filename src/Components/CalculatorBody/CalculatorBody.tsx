eeaba15e19eebd3de1fd12629861164ea0ba2d5c
Alwin Paul <alwin.paul7489@gmail.com>
2022-05-26 23:06:52 +0530

    shift to use reducer

--- src/Components/CalculatorBody/CalculatorBody.tsx
+++ src/Components/CalculatorBody/CalculatorBody.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import Display from "./../Display/Display.tsx";
 import ButtonPanel from "./../ButtonPanel/ButtonPanel.tsx";
 
@@ -6,74 +6,115 @@ import "./CalculatorBody.scss";
 
 function CalculatorBody() {
 
-    const [firstOperand, setFirstOperand] = useState("");
-    const [nextOperand, setNextOperand] = useState("");
-    const [operator, setOperator] = useState("");
-    const [operations, setOperations] = useState("");
-    const [result, setResult] = useState("");
+    // const [firstOperand, setFirstOperand] = useState("");
+    // const [nextOperand, setNextOperand] = useState("");
+    // const [operator, setOperator] = useState("");
+    // const [operations, setOperations] = useState("");
+    // const [result, setResult] = useState("");
+
+    const calculatorInitialState = {
+        firstOperand: "",
+        nextOperand: "",
+        operator: "",
+        operations: "",
+        result: ""
+    }
+
+    function calculatorReducer(state, action) {
+        switch (action.type) {
+          case 'reset':
+            return calculatorInitialState;
+          case 'setOperations':
+            return { ...state, operations: action.value };
+          case 'setNextOperand':
+            return { ...state, nextOperand: action.value };
+          case 'setFirstOperand':
+            return { ...state, firstOperand: action.value };
+          case 'setOperator':
+            return { ...state, operator: action.value };
+          case 'setResult':
+                return { ...state, result: action.value};
+          default:
+            throw new Error();
+        }
+      }
+
+    const [state, dispatch] = useReducer(calculatorReducer, calculatorInitialState);
 
     const operators = ["+", "-", "*", "/"];
-
-    // RESET CALCULATOR
-    function resetCalc() {
-        setFirstOperand("")
-        setNextOperand("")
-        setOperator("")
-        setOperations("")
-        setResult("")
-    }
 
     // HANDLE BUTTON CLICKS
     function handleButtonClick(val) {
         // RESET
         if(val === 'reset') {
-            resetCalc()
+            dispatch({type: 'reset'})
             return;
         }
         // CALCULATE RESULT
         if(val === "=") {
-            setOperations((prevVal) => {
-                return prevVal + nextOperand
+            dispatch({
+                type: 'setOperations',
+                value: state.operations + state.nextOperand
             })
             calculateResult()
             return;
         }
         // APPEND NUMBERS
         if(Number.isInteger(parseInt(val)) || val === ".") {
-            setNextOperand((prevVal) => {
-                return prevVal+val
+            dispatch({
+                type: 'setNextOperand',
+                value: state.nextOperand + val
             })
             return;
         }
         // OPERATIONS
         if(operators.includes(val)) {
-            setOperations((prevVal) => {
-                return prevVal + nextOperand + val
+            dispatch({
+                type: 'setOperations',
+                value: state.operations + state.nextOperand + val
             })
-            if(firstOperand && nextOperand) {
+            if(state.firstOperand && state.nextOperand) {
                 calculateResult()
             } else {
-                nextOperand && setFirstOperand(nextOperand)
-                setNextOperand("")
+                state.nextOperand && dispatch({
+                    type: 'setFirstOperand',
+                    value: state.nextOperand
+                })
+                dispatch({
+                    type: 'setNextOperand',
+                    value: ""
+                })
             }
-            setOperator(val)
+            dispatch({
+                type: 'setOperator',
+                value: val
+            })
         }
     }
 
     // CALCULATE RESULT
     function calculateResult() {
-        if(firstOperand && nextOperand) {
-            const result = eval(`${firstOperand} ${operator} ${nextOperand}`)
-            setResult(result)
-            setFirstOperand(result)
-            setNextOperand("")
+        if(state.firstOperand && state.nextOperand) {
+            let result =eval(`${state.firstOperand} ${state.operator} ${state.nextOperand}`);
+            dispatch({
+                type: "setResult",
+                value: result
+            })
+            dispatch({
+                type: "setFirstOperand",
+                value: result
+            })
+            dispatch({
+                type: "setNextOperand",
+                value: ""
+            })
         }
     }
 
     return (
         <div className="main-body">
             <div className="mb--container">
-                <Display total={nextOperand || result} operation={operations}></Display>
+                <Display total={state.nextOperand || state.result} operation={state.operations}></Display>
                 <ButtonPanel buttonClicked={handleButtonClick}></ButtonPanel>
             </div>
         </div>
